919a9333e7c179ede27e8bdc0ae5230e35af8e4c
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2024-11-13 14:54:23 +0530

    FEAT - added google analytics

--- ashharScript.js
+++ ashharScript.js
@@ -36,3 +36,24 @@ window.addEventListener('load', function() {
       mainImage.classList.add('activateHover');
   }, 2000); // 3000 milliseconds = 3 seconds
 });
+
+// Load Google Analytics Script Dynamically
+if (typeof GA_MEASUREMENT_ID !== 'undefined' && GA_MEASUREMENT_ID) {
+  // Load the Google Analytics library
+  const script1 = document.createElement('script');
+  script1.async = true;
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(script1);
+
+  // Initialize Google Analytics
+  const script2 = document.createElement('script');
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_MEASUREMENT_ID}');
+  `;
+  document.head.appendChild(script2);
+}
+
+
